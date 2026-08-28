7254dac220d4547e29e38e83187584d5401c831c
Suchith Chandan Reddy Pinreddy <70670543+pinreddysu@users.noreply.github.com>
2026-08-28 17:43:43 -0400

    Time: 248 ms (17.01%) | Memory: 19.8 MB (40.00%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,6 +1,9 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        #Brute force: generating all the subarrays that unique
+        return self.betterSolution(s)
+        # return self.bruteForceSolution(s)
+    #Brute force: generating all the subarrays that unique
+    def bruteForceSolution(self, s):
         maxLength = 0
         for i in range(len(s)):
             subString = ""
@@ -11,5 +14,24 @@ class Solution:
                 elif s[j] in subString:
                     break
         return maxLength
+    
+    #Better Solution: expand when condition is met and shrink when its not
+    def betterSolution(self, s):
+        l, r, subString = 0, 0, ""
+        maxLength = 0
+
+        while r < len(s):
+            
+            # print(subString)
+            while s[r] in subString:
+                subString = subString[1:]
+                l+=1
+            else:
+                subString+=s[r]
+                maxLength = max(maxLength, r-l+1)
+
+            r += 1
+        return maxLength
+
 
         
