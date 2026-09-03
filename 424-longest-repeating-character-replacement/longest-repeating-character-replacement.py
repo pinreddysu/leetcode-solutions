2e2a1ae9a7253f0fb6d84213072f4773fcf06391
Suchith Chandan Reddy Pinreddy <70670543+pinreddysu@users.noreply.github.com>
2026-09-03 17:36:46 -0400

    Time: 267 ms (6.78%) | Memory: 19.7 MB (16.15%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,38 +1,40 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        
-        # maxLen = 0
+        return self.optimizedSolution(s,k)
+    def bruteForceSolution(self, s, k):    
+        maxLen = 0
 
-        # for i in range(len(s)):
-        #     hashArr = [0] * 26
-        #     for j in range(i, len(s)):
-        #         hashArr[ord(s[j]) - ord('A')] +=1
+        for i in range(len(s)):
+            hashArr = [0] * 26
+            for j in range(i, len(s)):
+                hashArr[ord(s[j]) - ord('A')] +=1
 
-        #         if j-i+1 -(max(hashArr)) <=k:
-        #             maxLen = max(maxLen, j-i+1)
-        #         else:
-        #             break
-        # print(maxLen)
-        # return maxLen
-    
-        # l = 0
-        # r = 0
-        # maxLen = 0
-        # hashArr = [0] * 26
+                if j-i+1 -(max(hashArr)) <=k:
+                    maxLen = max(maxLen, j-i+1)
+                else:
+                    break
+        print(maxLen)
+        return maxLen
+    def betterSolution(self, s, k):
+        l = 0
+        r = 0
+        maxLen = 0
+        hashArr = [0] * 26
 
-        # while r < len(s):
-        #     hashArr[ord(s[r]) - ord('A')] += 1
+        while r < len(s):
+            hashArr[ord(s[r]) - ord('A')] += 1
 
-        #     while r - l + 1 - max(hashArr) > k:
-        #         hashArr[ord(s[l]) - ord('A')] -= 1
-        #         l+=1
+            while r - l + 1 - max(hashArr) > k:
+                hashArr[ord(s[l]) - ord('A')] -= 1
+                l+=1
             
-        #     if r-l+1 - max(hashArr) <= k:
-        #         maxLen = max(maxLen, r-l+1)
-        #         r+=1
-        # print(maxLen)
-        # return maxLen
+            if r-l+1 - max(hashArr) <= k:
+                maxLen = max(maxLen, r-l+1)
+                r+=1
+        print(maxLen)
+        return maxLen
 
+    def optimizedSolution(self, s, k):
         l = 0
         r = 0
         maxLen = 0
