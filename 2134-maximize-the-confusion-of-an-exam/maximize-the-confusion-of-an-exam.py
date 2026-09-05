1e51615db487ce9e21785c014ed6b921929e6bcb
Suchith Chandan Reddy Pinreddy <70670543+pinreddysu@users.noreply.github.com>
2026-09-04 23:26:51 -0400

    Time: 359 ms (5.06%) | Memory: 19.6 MB (40.48%) - LeetSync

---
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        l = 0
+        r = 0
+        maxLen = 0
+        hashMap = {}
+
+        while r < len(answerKey):
+            if answerKey[r] not in hashMap.keys():
+                hashMap[answerKey[r]] = 1
+            else:
+                hashMap[answerKey[r]] += 1
+            
+            while r-l+1 - max(hashMap.values()) > k:
+                hashMap[answerKey[l]] -= 1
+                l+=1
+            if r-l+1 - max(hashMap.values()) <= k:
+                maxLen = max(maxLen, r-l+1)
+                r+=1
+        print(maxLen)
+        return maxLen
