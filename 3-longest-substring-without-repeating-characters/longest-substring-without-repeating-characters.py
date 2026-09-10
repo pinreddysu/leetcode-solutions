ca453cab01a573539315ec2046f4561cf812fec8
Suchith Chandan Reddy Pinreddy <70670543+pinreddysu@users.noreply.github.com>
2026-09-09 22:07:40 -0400

    Time: 1519 ms (5.00%) | Memory: 20 MB (8.41%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,44 +1,43 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        return self.optimizedSolution(s)
-        # return self.betterSolution(s)
+        '''
+        What is my window?
+        -> My window is variable as we are finding largest substring with unique
+        characters meaning finding largest window where all of them are unique
+        What makes it invalid?
+        -> Finding a repeated character
+        When do I move right?
+        -> I move right when there is unique character
+        When do I move left?
+        -> I move left until I remove the duplicate character
+        What does my hashmap/count store?
+        -> I store the frequency because to know how many times a character
+        has repeated
+        Am I finding one window or counting multiple?
+        -> Finding the longest window which has distinct characters
+        '''
         # return self.bruteForceSolution(s)
-    #Brute force: generating all the subarrays that unique
+        return self.betterSolution(s)
+
     def bruteForceSolution(self, s):
-        maxLength = 0
+        maxLen = 0
         for i in range(len(s)):
-            subString = ""
-            for j in range(i,len(s)):
-                if not s[j] in subString:
-                    subString += s[j]
-                    maxLength = max(len(subString), maxLength)
-                elif s[j] in subString:
+            hashSet = set()
+            for j in range(i, len(s)):
+                if s[j] not in hashSet:
+                    hashSet.add(s[j])
+                    maxLen = max(maxLen, j-i+1)
+                else:
                     break
-        return maxLength
+
+        print(maxLen)
+        return maxLen
     
-    #Better Solution: expand when condition is met and shrink when its not
-    #if we use string the TC is o^2 because for subString[1:] there's a loop running to copy it so use set that way O(n) and o(1)
     def betterSolution(self, s):
-        l, r, subStringSet = 0, 0, set()
-        maxLength = 0
-
-        while r < len(s):
-            
-            while s[r] in subStringSet:
-                subStringSet.remove(s[l])
-                l+=1
-            else:
-                subStringSet.add(s[r])
-                maxLength = max(maxLength, r-l+1)
-
-            r += 1
-        return maxLength
-
-    def optimizedSolution(self, s):
-        l = 0
-        r = 0
         hashMap = {}
         maxLen = 0
+        l = 0
+        r = 0
 
         while r < len(s):
             if s[r] not in hashMap.keys():
@@ -46,8 +45,8 @@ class Solution:
             else:
                 hashMap[s[r]] += 1
 
-            if max(hashMap.values()) > 1:
-                hashMap[s[l]] -=1
+            while max(hashMap.values()) > 1:
+                hashMap[s[l]] -= 1
                 if hashMap[s[l]] == 0:
                     hashMap.pop(s[l])
                 l+=1
@@ -55,11 +54,4 @@ class Solution:
             if max(hashMap.values()) <= 1:
                 maxLen = max(maxLen, r-l+1)
             r+=1
-        print(maxLen)
         return maxLen
-    
-
-
-
-
-        
