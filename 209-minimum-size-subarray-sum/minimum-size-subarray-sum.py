719304486017959621fd22f3287517b4781a5351
Suchith Chandan Reddy Pinreddy <70670543+pinreddysu@users.noreply.github.com>
2026-09-09 22:38:47 -0400

    Time: 27 ms (14.04%) | Memory: 30.5 MB (79.00%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,61 +1,49 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        '''
+        What is my window?
+        -> My window is variable and it is integers that are >= target
+        What makes it invalid?
+        -> When my window reaches the sum of subarray > target and len of current
+        window is greater than min window that we found 
+        When do I move right?
+        -> We expand it to add integers and to find min window
+        When do I move left?
+        -> We move left when target <= subarray and if current window > min window
+        What does my hashmap/count store?
+        -> We don't use hashMap we just use sum varialbe to increment or decrement
+        the value when moving right or left
+        Am I finding one window or counting multiple?
+        -> we are finding one window which has min subarray length
+        '''
         # return self.bruteForceSolution(target, nums)
-        # return self.betterSolution(target, nums)
-        return self.optimalSolution(target, nums)
-    #Bruteforce does not work because of time limit exceeded
+        return self.betterSolution(target, nums)
     def bruteForceSolution(self, target, nums):
-        minLength = math.inf
-
+        minLen = math.inf
         for i in range(len(nums)):
             targetSum = 0
-            for j in range(i,len(nums)):
+            for j in range(i, len(nums)):
                 targetSum += nums[j]
                 if targetSum >= target:
-                    print(j,i)
-                    minLength = min(minLength, j-i+1)
+                    minLen = min(minLen, j-i+1)
                     break
-        if(minLength == math.inf):
-            return 0
-        return minLength
+        return 0 if minLen == math.inf else minLen
 
     def betterSolution(self, target, nums):
-        l, r, targetSum = 0, 0, 0
-        minLength = math.inf
-
-        while l < len(nums):
-            if r < len(nums):
-                targetSum += nums[r]
-            while targetSum >= target:
-                minLength = min(minLength, r-l+1)
-                targetSum-=nums[l]
-                l+=1
-            
-            if targetSum < target and r == len(nums):
-                l+=1
-            
-            if r != len(nums):
-                r+=1
-        if minLength == math.inf:
-            return 0
-        else:
-            return minLength
-    def optimalSolution(self, target, nums):
         l = 0
         r = 0
-        targetSum = math.inf
-        sumSubArray = 0
+        minLen = math.inf
+        targetSum = 0
 
         while r < len(nums):
-            sumSubArray += nums[r]
+            targetSum += nums[r]
 
-            while sumSubArray >= target:
-                targetSum = min(targetSum, r-l+1)
-                sumSubArray -= nums[l]
+            while targetSum >= target:
+                minLen = min(minLen, r-l+1)
+                targetSum -= nums[l]
                 l+=1
-            
+                
             r+=1
-        print(targetSum)
-        if targetSum == math.inf:
-            return 0
-        return targetSum
+
+        print(minLen)
+        return 0 if minLen == math.inf else minLen
