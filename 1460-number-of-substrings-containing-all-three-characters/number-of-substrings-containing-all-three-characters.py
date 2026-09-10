b3d9de3b91554c341875df6551b05bc1e7bef2c4
Suchith Chandan Reddy Pinreddy <70670543+pinreddysu@users.noreply.github.com>
2026-09-10 00:07:44 -0400

    Time: 93 ms (64.21%) | Memory: 19.5 MB (16.90%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -1,56 +1,44 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
+        '''
+        What is my window?
+        -> It is variable window which consist of all three characters atleast once
+        What makes it invalid?
+        -> when there is a missing occurrences of a character
+        When do I move right?
+        -> To find occurences of all three letters
+        When do I move left?
+        -> We move left when we found another occurence of the character that we already found
+        What does my hashmap/count store?
+        -> we will use hashArray to store position of last occurrences to count 
+        substrings
+        Am I finding one window or counting multiple?
+        -> we are counting mutiple windows
+        '''
+        # return self.bruteForceSolution(s)
         return self.betterSolution(s)
-    
-    def bruteForceSolution(self,s):
-        totalSubStrings = 0
+
+    def bruteForceSolution(self, s):
+        count = 0
         for i in range(len(s)):
             hashSet = set()
             for j in range(i, len(s)):
                 hashSet.add(s[j])
                 if len(hashSet) == 3:
-                    totalSubStrings += 1
-        print(totalSubStrings)
-        return totalSubStrings
-    
-    def bruteForceSolutionOptimized(self, s):
-        totalSubStrings = 0
-        for i in range(len(s)):
-            hashSet = set()
-            for j in range(i, len(s)):
-                hashSet.add(s[j])
-                if len(hashSet) == 3:
-                    totalSubStrings += len(s) - j
-                    break
-        print(totalSubStrings)
-        return totalSubStrings
+                    count+=1
+        return count
     
     def betterSolution(self, s):
-        totalSubStrings = 0
         l = 0
         r = 0
-        hashMap = {}
+        hashArr = [-1] * 3
+        count = 0
+
         while r < len(s):
-            if s[r] not in hashMap.keys():
-                hashMap[s[r]] = 1
-            else:
-                hashMap[s[r]] += 1
-            
-            if len(hashMap.keys()) == 3:
-                totalSubStrings += len(s) - r
-            
-            while len(hashMap.keys()) == 3:
-                hashMap[s[l]] -= 1
-                if hashMap[s[l]] == 0:
-                    hashMap.pop(s[l])
-                if len(hashMap.keys()) == 3:
-                    totalSubStrings += len(s) - r
-                l+=1
-            
-            if len(hashMap.keys()) < 3:
-                r+=1
-        print(totalSubStrings)
-        return totalSubStrings
+            hashArr[ord(s[r]) - ord('a')] = r
 
+            if min(hashArr) > -1:
+                count+= min(hashArr) -l +1
+            r+=1
+        return count
 
-       
