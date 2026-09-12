e64db7c6b1ba02bf23ffb601214119d9e356abf0
Suchith Chandan Reddy Pinreddy <70670543+pinreddysu@users.noreply.github.com>
2026-09-12 15:21:12 -0400

    Time: 131 ms (24.18%) | Memory: 35.3 MB (20.68%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,7 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        return self.bruteForceSolution(nums, k)
+        # return self.bruteForceSolution(nums, k)
+        return self.optimizedSolution(nums, k)
 
     def bruteForceSolution(self, nums, k):
         rotations = k % len(nums)
@@ -15,3 +16,13 @@ class Solution:
             for i in range(rotations):
                 nums[i] = rightKElements[i]
             print(nums)
+    def optimizedSolution(self, nums, k):
+        rotations = k % len(nums)
+        def reverse(l, r):
+            while l < r:
+                nums[l],  nums[r] = nums[r], nums[l]
+                l+=1
+                r-=1
+        reverse(0, len(nums)-1)
+        reverse(0, rotations-1)
+        reverse(rotations, len(nums)-1)
