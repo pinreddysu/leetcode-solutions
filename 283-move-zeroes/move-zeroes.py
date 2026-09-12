72c9e92b680cfe052c936ca3caec55c7d7a59c36
Suchith Chandan Reddy Pinreddy <70670543+pinreddysu@users.noreply.github.com>
2026-09-12 16:33:21 -0400

    Time: 4 ms (59.08%) | Memory: 20.7 MB (5.19%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,9 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        self.bruteForceSolution(nums)
+        # self.bruteForceSolution(nums)
+        self.optimizedSolution(nums)
 
-        
     def bruteForceSolution(self, nums):
         nonZeros = []
 
@@ -18,5 +18,20 @@ class Solution:
 
         for i in range(len(nonZeros), len(nums)):
             nums[i] = 0
+        print(nums)
+    
+    def optimizedSolution(self, nums):
+        l = -1
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                l = i
+                break
+        if l != -1:
+            r = l+1
+            while r < len(nums):
+                if nums[r] != 0:
+                    nums[l], nums[r] = nums[r], nums[l]
+                    l+=1
+                r+=1
         print(nums)
         
