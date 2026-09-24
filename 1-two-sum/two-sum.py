7dc7ab9fc2ab196f6c3ff197647ddc51ca3a5909
Suchith Chandan Reddy Pinreddy <70670543+pinreddysu@users.noreply.github.com>
2026-09-23 21:32:45 -0400

    Time: 1714 ms (27.75%) | Memory: 19.9 MB (66.95%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,6 +1,16 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        return self.optimalSolution(nums, target)
+        # return self.optimalSolution(nums, target)
+        return self.bruteForceSolution(nums, target)
+        
+    def bruteForceSolution(self, nums, target):
+
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return i, j
+
+
     def optimalSolution(self, nums, target):
         #Prefix sum
         hashMap = {}
