8738d9c46db5dd016bea4edadac372d6920134ad
Suchith Chandan Reddy Pinreddy <70670543+pinreddysu@users.noreply.github.com>
2026-09-24 23:16:12 -0400

    Time: 8 ms (47.25%) | Memory: 21.7 MB (6.15%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,7 +1,8 @@
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
         # return self.bruteForceSolution(nums)
-        return self.betterSolution(nums)
+        # return self.betterSolution(nums)
+        return self.optimalSolution(nums)
     def bruteForceSolution(self, nums):
         for i in range(len(nums)):
             count = 0
@@ -22,3 +23,24 @@ class Solution:
         for i in hashMap.keys():
             if hashMap[i] > len(nums) // 2:
                 return i
+    
+    def optimalSolution(self, nums):
+        count = 0
+        for i in nums:
+            if count == 0:
+                element = i
+            if element == i:
+                count += 1
+            else:
+                count -= 1
+        
+        return element
+        
+        # if they stated not to assume there exist majority element then
+        count = 0
+        for i in nums:
+            if i == element:
+                count+=1
+        if count > len(nums)//2:
+            return element
+        return -1
